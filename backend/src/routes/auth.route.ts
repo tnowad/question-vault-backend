2bdd71a50d688d97db81edbfb7ec3f1055200b21
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-04 21:46:48 +0700

    feat(route): add asyncHandler to prevent warning

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,22 +1,18 @@
 import { Router } from "express";
 import { Routes } from "../interfaces/routes.interface";
+import { AuthController } from "../controllers/auth.controller";
+import asyncHandler from "express-async-handler";
 
 export class AuthRoute implements Routes {
   public path = "/api/auth";
   public router = Router();
-
+  public auth = new AuthController();
   constructor() {
     this.initializeRoutes();
   }
   private initializeRoutes() {
-    this.router.post(`${this.path}/register`, (req, res) => {
-      res.status(200).json({ message: "register" });
-    });
-    this.router.post(`${this.path}/login`, (req, res) => {
-      res.status(200).json({ message: "login" });
-    });
-    this.router.post(`${this.path}/logout`, (req, res) => {
-      res.status(200).json({ message: "logout" });
-    });
+    this.router.post(`${this.path}/register`, asyncHandler(this.auth.register));
+    this.router.post(`${this.path}/login`, asyncHandler(this.auth.login));
+    this.router.post(`${this.path}/logout`, asyncHandler(this.auth.logout));
   }
 }
