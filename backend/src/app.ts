54f2bf3aebb00e2f04d7f7bdc7627ce1d4d90ee7
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-04 14:18:54 +0700

    feat(config): setup read port from .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { PORT } from "../config";
 export class App {
   public app: express.Application;
   public port: string | number;
   constructor() {
     this.app = express();
-    this.port = 3000;
+    this.port = PORT ?? 3000;
   }
   public listen() {
     this.app.listen(this.port, () => {
