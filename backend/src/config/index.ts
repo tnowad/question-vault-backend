748cac2f9c4a9c47bb1c75cf6c53f8f8874ff566
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-05 02:58:28 +0700

    feat(env): add config env for jwt

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,4 +2,13 @@ import { config } from "dotenv";
 
 config({ path: ".env" });
 
-export const { PORT, LOG_DIR, LOG_FORMAT, ORIGIN, CREDENTIALS } = process.env;
+export const {
+  PORT,
+  LOG_DIR,
+  LOG_FORMAT,
+  ORIGIN,
+  CREDENTIALS,
+  JWT_SECRET,
+  ACCESS_TOKEN_EXPIRATION,
+  REFRESH_TOKEN_EXPIRATION,
+} = process.env;
