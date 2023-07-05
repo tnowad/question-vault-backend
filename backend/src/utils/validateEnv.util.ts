f864e34a15d5894ac4d0488901a9383af2493a1f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-07-05 16:28:09 +0700

    feat(env): setup clean env

--- backend/src/utils/validateEnv.util.ts
+++ backend/src/utils/validateEnv.util.ts
@@ -1,12 +1,15 @@
 import { cleanEnv, port, str, bool } from "envalid";
 
 export const validateEnv = () => {
-  cleanEnv(process.env, {
+  return cleanEnv(process.env, {
     PORT: port(),
     DATABASE_URL: str(),
     LOG_FORMAT: str(),
     LOG_DIR: str(),
     ORIGIN: str(),
     CREDENTIALS: bool(),
+    JWT_SECRET: str(),
+    ACCESS_TOKEN_EXPIRATION: str(),
+    REFRESH_TOKEN_EXPIRATION: str(),
   });
 };
