63e286b87833dd3e8055c346a6dc55afb6e3f0c5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-20 05:01:16 +0700

    feat: improve response in AppController and extend AppService data

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      message: 'Hello World!',
+      date: new Date(),
+      version: '1.0.0',
+      environment: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
+      port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    };
   }
 }
