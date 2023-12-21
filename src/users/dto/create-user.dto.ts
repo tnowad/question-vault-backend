b94e1b6b6a602b07db1d0b416ab5ce82612a90dc
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-21 20:02:33 +0700

    feat: add create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto { }
+import {
+  IsEmail,
+  IsOptional,
+  MaxLength,
+  IsDate,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  fullName?: string;
+
+  @IsOptional()
+  @MaxLength(32)
+  phone?: string;
+
+  @IsOptional()
+  @MaxLength(32)
+  username?: string;
+
+  @IsEmail()
+  email: string;
+
+  @MaxLength(128)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one digit',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsDate()
+  birthdate?: Date;
+}
