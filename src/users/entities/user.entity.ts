85ece0fefbaa8a9e6217a3e14a1c1188403821b5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-21 20:01:58 +0700

    feat: add user entity and validate fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User { }
+import { Exclude } from 'class-transformer';
+import { IsOptional, MaxLength, IsDate, IsEmail } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  fullName: string;
+
+  @Column({ length: 32, nullable: true })
+  @MaxLength(32)
+  phone: string;
+
+  @Column({ length: 32, nullable: true, unique: true })
+  @MaxLength(32)
+  username: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column({ length: 128 })
+  @MaxLength(128)
+  @IsOptional()
+  @Exclude()
+  password: string;
+
+  @Column({ type: 'date', nullable: true })
+  @IsOptional()
+  @IsDate()
+  birthdate: Date;
+}
