1b35e2283d3431d4551d3fd1a54b1fa35ad0794d
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-22 20:05:12 +0700

    feat: update validation rules in update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,39 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsOptional,
+  MaxLength,
+  IsDate,
+  IsNotEmpty,
+  IsString,
+  Matches,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) { }
+export class UpdateUserDto {
+  @IsOptional()
+  fullName?: string;
+
+  @IsOptional()
+  @MaxLength(32)
+  phone?: string;
+
+  @IsOptional()
+  @MaxLength(32)
+  username?: string;
+
+  @IsOptional()
+  @IsDate()
+  birthdate?: Date;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  oldPassword?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one digit',
+  })
+  newPassword?: string;
+}
