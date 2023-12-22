6b0323dc105537e85c778019945c938d0b99f229
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-22 20:14:07 +0700

    feat: add user creation in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
+
+const saltRounds = 10;
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    createUserDto.password = await bcrypt.hash(
+      createUserDto.password,
+      saltRounds,
+    );
+
+    const user = this.usersRepository.create(createUserDto);
+    await this.usersRepository.save(user);
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'User created successfully',
+    };
   }
 
   findAll() {
