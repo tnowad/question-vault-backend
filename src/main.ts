2f38d8d684bfdd174caab763405a0bfd2226c34c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-23 22:45:48 +0700

    feat: setup swagger for project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development' ? '*' : process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Question Vault')
+    .setDescription('The Question Vault API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
