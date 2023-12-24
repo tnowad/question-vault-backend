3ca1f7289fa79ddb8d3d3629cf91b0ce5fdebcee
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-24 11:58:28 +0700

    feat: add Swagger annotations to CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsOptional,
@@ -7,20 +8,39 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ required: false, example: 'John Doe' })
   @IsOptional()
   fullName?: string;
 
+  @ApiProperty({
+    maxLength: 32,
+    format: 'phone',
+    required: false,
+    example: '1234567890',
+  })
   @IsOptional()
   @MaxLength(32)
   phone?: string;
 
+  @ApiProperty({ maxLength: 32, required: false, example: 'john_doe' })
   @IsOptional()
   @MaxLength(32)
   username?: string;
 
+  @ApiProperty({
+    maxLength: 128,
+    format: 'email',
+    required: true,
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    maxLength: 128,
+    pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$',
+    example: 'Password123',
+  })
   @MaxLength(128)
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
     message:
@@ -28,6 +48,7 @@ export class CreateUserDto {
   })
   password: string;
 
+  @ApiProperty({ type: Date, required: false, example: '2022-01-01' })
   @IsOptional()
   @IsDate()
   birthdate?: Date;
