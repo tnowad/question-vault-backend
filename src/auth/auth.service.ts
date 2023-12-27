5c28b7b34c11f2c02af1149837af59080eba3346
Nguyen Minh Tuan <tnowad@gmail.com>
2023-12-27 15:15:46 +0700

    feat: add validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcryptjs';
 import { SignInDto } from './dto/sign-in.dto';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { SignUpDto } from './dto/sign-up.dto';
 import { JwtService } from '@nestjs/jwt';
+import { Cache } from 'cache-manager';
+import { User } from 'src/users/entities/user.entity';
+import { UserNotFoundException } from 'src/common/exceptions/user-not-found.exception';
 
 @Injectable()
 export class AuthService {
@@ -12,74 +14,24 @@ export class AuthService {
     private usersService: UsersService,
     private jwtService: JwtService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
-  ) { }
+  ) {}
 
-  async signIn(signInDto: SignInDto) {
-    const user = await this.usersService.findOne({
-      email: signInDto.email,
-    });
+  async validateUser(signInDto: SignInDto): Promise<User> {
+    const { email, password } = signInDto;
+    const user = await this.usersService.findOne({ email });
 
     if (!user) {
-      throw new HttpException(
-        {
-          status: HttpStatus.UNPROCESSABLE_ENTITY,
-          errors: {
-            email: 'User not found',
-          },
-        },
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+      throw new UserNotFoundException();
     }
 
-    const isPasswordValid = await bcrypt.compare(
-      signInDto.password,
-      user.password,
-    );
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      throw new HttpException(
-        {
-          status: HttpStatus.UNPROCESSABLE_ENTITY,
-          errors: {
-            password: 'Incorrect password',
-          },
-        },
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+      throw new UserNotFoundException();
     }
 
-    const payload = { sub: user.id, email: user.email };
+    // Todo: Update last login date
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Login successful',
-      access_token: await this.jwtService.signAsync(payload),
-    };
-  }
-
-  async signUp(signUpDto: SignUpDto) {
-    const candidate = await this.usersService.findOne({
-      email: signUpDto.email,
-    });
-
-    if (candidate) {
-      throw new HttpException(
-        {
-          status: HttpStatus.UNPROCESSABLE_ENTITY,
-          errors: {
-            email: 'User already exists',
-          },
-        },
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
-    }
-
-    const user = await this.usersService.create(signUpDto);
-
-    return {
-      statusCode: HttpStatus.CREATED,
-      message: 'User created successfully',
-      user,
-    };
+    return user;
   }
 }
