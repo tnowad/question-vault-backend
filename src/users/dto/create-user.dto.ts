eead303c157a4b57bc6ac5b411c3068316e5cf2a
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-02 00:00:02 +0700

    feat: add field username and remove field email and password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsOptional,
-  MaxLength,
-  IsDate,
-  Matches,
-} from 'class-validator';
+import { IsOptional, MaxLength, IsDate } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ required: false, example: 'John Doe' })
@@ -22,31 +16,10 @@ export class CreateUserDto {
   @MaxLength(32)
   phone?: string;
 
-  @ApiProperty({ maxLength: 32, required: false, example: 'john_doe' })
+  @ApiProperty({ maxLength: 32, required: true, example: 'john_doe' })
   @IsOptional()
   @MaxLength(32)
-  username?: string;
-
-  @ApiProperty({
-    maxLength: 128,
-    format: 'email',
-    required: true,
-    example: 'john.doe@example.com',
-  })
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({
-    maxLength: 128,
-    pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$',
-    example: 'Password123',
-  })
-  @MaxLength(128)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
-    message:
-      'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one digit',
-  })
-  password: string;
+  username: string;
 
   @ApiProperty({ type: Date, required: false, example: '2022-01-01' })
   @IsOptional()
