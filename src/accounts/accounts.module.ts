9fcf38630dcafcf98dfcd2e66eb0372cf66bc6ef
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-03 11:00:59 +0700

    feat: add account service

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Account])],
+  providers: [AccountsService],
+})
 export class AccountsModule {}
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(
+    @InjectRepository(Account) private accountsRepository: Repository<Account>,
+  ) {}
+}
