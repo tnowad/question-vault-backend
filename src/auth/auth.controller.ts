d8f6c2e719219e5acbbd1f44556dfe11867c1d62
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-03 11:02:25 +0700

    refactor: remove not used code in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDto } from './dto/sign-in.dto';
 import { ApiTags } from '@nestjs/swagger';
-import { SignUpDto } from './dto/sign-up.dto';
-import { TokenPayloadDto } from './dto/token-payload.dto';
-import { SignInPayloadDto } from './dto/sign-in-payload.dto';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Post('sign-in')
-  async signIn(@Body() signInDto: SignInDto) {
-    const user = await this.authService.validateUser(signInDto);
-
-    const token = new TokenPayloadDto({
-      accessToken: '',
-      refreshToken: '',
-    });
-
-    return new SignInPayloadDto(user, token);
-  }
-
-  @Post('sign-up')
-  signUp(@Body() signUpDto: SignUpDto) {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import * as bcrypt from 'bcryptjs';
-import { SignInDto } from './dto/sign-in.dto';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { JwtService } from '@nestjs/jwt';
 import { Cache } from 'cache-manager';
-import { User } from 'src/users/entities/user.entity';
-import { UserNotFoundException } from 'src/common/exceptions/user-not-found.exception';
 
 @Injectable()
 export class AuthService {
@@ -15,23 +11,4 @@ export class AuthService {
     private jwtService: JwtService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
-
-  async validateUser(signInDto: SignInDto): Promise<User> {
-    const { email, password } = signInDto;
-    const user = await this.usersService.findOne({ email });
-
-    if (!user) {
-      throw new UserNotFoundException();
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      throw new UserNotFoundException();
-    }
-
-    // Todo: Update last login date
-
-    return user;
-  }
 }
