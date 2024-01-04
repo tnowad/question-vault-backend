4afd6ea5640bb41840a11ff3252fb402623b9d19
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-04 12:25:51 +0700

    feat: add function findOne in AccountsService

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class AccountsService {
   constructor(
     @InjectRepository(Account) private accountsRepository: Repository<Account>,
   ) {}
+
+  async findOne(fields: FindOptionsWhere<Account>): Promise<Account> {
+    const account = await this.accountsRepository.findOneBy(fields);
+
+    if (account === null) {
+      throw new BadRequestException('Account not found');
+    }
+
+    return account;
+  }
 }
