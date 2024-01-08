6b3c1d933f1ab2921cf770b7dd239d8bb01b3d92
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-09 00:54:39 +0700

    fix: add import service and export service

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), UsersModule],
   providers: [AccountsService],
+  exports: [AccountsService],
 })
 export class AccountsModule {}
