c1dcfdb29c9abe34376d67fe77bb8d80a8dd868e
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-16 07:18:15 +0700

    feat: add endpoint `sign-in` and `callback`

--- src/auth-github/auth-github.controller.ts
+++ src/auth-github/auth-github.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthGithubService } from './auth-github.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('auth-github')
-export class AuthGithubController {}
+@ApiTags('auth/github')
+@Controller('auth/github')
+export class AuthGithubController {
+  constructor(private readonly authGithubService: AuthGithubService) {}
+
+  @Post('sign-in')
+  async signIn() {}
+
+  @Post('callback')
+  async callback() {}
+}
