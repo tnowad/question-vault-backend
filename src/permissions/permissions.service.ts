ae507dfd8f21e8ad7bf11c522a5dd67dacf4d0ca
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-17 07:19:20 +0700

    feat: add create permission method

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Permission } from './entities/permission.entity';
+import { CreatePermissionDto } from './dto/create-permission.dto';
 
 @Injectable()
-export class PermissionsService {}
+export class PermissionsService {
+  constructor(
+    @InjectRepository(Permission)
+    private readonly permissionsRepository: Repository<Permission>,
+  ) {}
+
+  async createPermission(
+    createPermissionDto: CreatePermissionDto,
+  ): Promise<Permission> {
+    const permission = this.permissionsRepository.create(createPermissionDto);
+    return this.permissionsRepository.save(permission);
+  }
+}
