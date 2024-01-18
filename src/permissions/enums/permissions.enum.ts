80cc81f11aa544da522d1e81ca1c82ed3cdd7952
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-18 08:17:35 +0700

    feat: add permission value enum

---
+++ src/permissions/enums/permissions.enum.ts
@@ -0,0 +1,6 @@
+export enum PermissionValue {
+  AUTH_EMAIL_SIGNIN = 'auth-email:sign-in',
+  AUTH_EMAIL_SIGNUP = 'auth-email:sign-up',
+  AUTH_EMAIL_FORGOT_PASSWORD = 'auth-email:forgot-password',
+  AUTH_EMAIL_RESET_PASSWORD = 'auth-email:reset-password',
+}
