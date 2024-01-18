2e1fc4297400a5b08ecdd5f62aac9bf76112ac70
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-18 08:17:25 +0700

    feat: remove unused fields

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,37 +1,15 @@
 import { Role } from 'src/roles/entities/role.entity';
-import {
-  Column,
-  CreateDateColumn,
-  DeleteDateColumn,
-  Entity,
-  ManyToMany,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { PermissionValue } from '../enums/permissions.enum';
 
 @Entity({ name: 'permissions' })
 export class Permission {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ type: String })
-  name: string;
-
-  @Column({ type: String })
-  description: string;
-
-  @Column({ type: String })
-  value: string;
+  @Column({ type: 'enum', enum: PermissionValue, unique: true })
+  value: PermissionValue;
 
   @ManyToMany(() => Role, (role: Role) => role.permissions)
   roles: Role[];
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @DeleteDateColumn()
-  deletedAt: Date;
 }
