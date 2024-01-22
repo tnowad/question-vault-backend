973d1fea61018f26c6bfc59aafd11c826dbc531d
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-22 10:37:30 +0700

    feat: add field in post entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,3 +1,49 @@
-import { Entity } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 @Entity()
-export class Post {}
+export class Post {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: String })
+  title: string;
+
+  @Column({ type: String })
+  body: string;
+
+  @Column({ type: Number })
+  viewCount: number;
+
+  @Column({ type: Number })
+  score: number;
+
+  @JoinColumn({ name: 'ownerUserId' })
+  ownerUser: User;
+
+  @Column({ type: Number })
+  ownerUserId: number;
+
+  @JoinColumn({ name: 'lastEditorUserId' })
+  lastEditorUser: User;
+
+  @Column({ type: Number })
+  lastEditorUserId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
