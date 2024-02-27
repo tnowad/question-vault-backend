0209df730683b6361335a21d05ea0fd511395410
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-28 00:23:46 +0700

    feat: make TypeOrmModule load dynamic config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './auth/constants';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AccountsModule } from './accounts/accounts.module';
 import { AuthEmailModule } from './auth-email/auth-email.module';
 import { AuthGithubModule } from './auth-github/auth-github.module';
@@ -25,16 +25,22 @@ import databaseConfig from './common/configs/database.config';
       isGlobal: true,
       load: [appConfig, databaseConfig, jwtConfig],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      synchronize: true,
-      autoLoadEntities: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get('database.host'),
+          port: config.get('database.port'),
+          username: config.get('database.username'),
+          password: config.get('database.password'),
+          database: config.get('database.database'),
+          synchronize: true,
+          autoLoadEntities: true,
+          logging: true,
+        };
+      },
     }),
     JwtModule.register({
       global: true,
