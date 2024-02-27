936359b765903a1278f0819328aed9e922bde830
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-28 04:11:16 +0700

    feat: add crud controller config

---
+++ src/configs/configs.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ConfigsService } from './configs.service';
+import { CreateConfigDto } from './dto/create-config.dto';
+import { UpdateConfigDto } from './dto/update-config.dto';
+import { Config } from './entities/config.entity';
+
+@Controller('configs')
+export class ConfigsController {
+  constructor(private readonly configsService: ConfigsService) {}
+
+  @Post()
+  async create(@Body() createConfigDto: CreateConfigDto): Promise<Config> {
+    return await this.configsService.create(createConfigDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Config[]> {
+    return await this.configsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Config> {
+    return await this.configsService.findOne(+id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateConfigDto: UpdateConfigDto,
+  ): Promise<Config> {
+    return await this.configsService.update(+id, updateConfigDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.configsService.remove(+id);
+  }
+}
--- src/configs/configs.module.ts
+++ src/configs/configs.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigsService } from './configs.service';
+import { ConfigsController } from './configs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Config } from './entities/config.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Config])],
+  controllers: [ConfigsController],
   providers: [ConfigsService],
 })
 export class ConfigsModule {}
