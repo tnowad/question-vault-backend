68363a403e42a64056c0260cfe7f4fced0cb4ddf
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-28 18:20:00 +0700

    feat: add create post dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,35 @@
-export class CreatePostDto {}
+import {
+  IsString,
+  IsNumber,
+  IsNotEmpty,
+  Min,
+  IsOptional,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  body: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  viewCount?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  score: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  ownerUserId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  lastEditorUserId: number;
+}
