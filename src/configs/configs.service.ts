33ee5e35cc2151863e4084806cdb40f6b3c3b1e9
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-03 07:49:28 +0700

    feat: add cache for configs service

--- src/configs/configs.service.ts
+++ src/configs/configs.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateConfigDto } from './dto/create-config.dto';
 import { UpdateConfigDto } from './dto/update-config.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Config } from './entities/config.entity';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import { Cache } from 'cache-manager';
 import { Inject } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
@@ -15,15 +15,20 @@ export class ConfigsService {
     private readonly configsRepository: Repository<Config>,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {}
+
   async create(createConfigDto: CreateConfigDto): Promise<Config> {
     const config = this.configsRepository.create(createConfigDto);
     const savedConfig = await this.configsRepository.save(config);
-    await this.cacheManager.del('configs');
+
+    this.cacheManager.store
+      .keys('configs::*')
+      .then((keys) => keys.forEach((key) => this.cacheManager.del(key)));
+
     return savedConfig;
   }
 
   async findAll(): Promise<Config[]> {
-    const cacheKey = 'configs';
+    const cacheKey = 'configs::all';
     const cachedConfigs = await this.cacheManager.get<Config[] | null>(
       cacheKey,
     );
@@ -32,39 +37,46 @@ export class ConfigsService {
     }
 
     const configs = await this.configsRepository.find();
-    await this.cacheManager.set(cacheKey, configs, 60);
+    await this.cacheManager.set(cacheKey, configs, 6000);
     return configs;
   }
 
-
-  async findOne(id: number): Promise<Config> {
-    const cacheKey = `config_${id}`;
+  async findOne(fields: FindOptionsWhere<Config>): Promise<Config | null> {
+    const cacheKey = `configs::${JSON.stringify(fields)}`;
     const cachedConfig = await this.cacheManager.get<Config | null>(cacheKey);
     if (cachedConfig) {
       return cachedConfig;
     }
 
-    const config = await this.configsRepository.findOneBy({ id });
-    if (!config) {
-      throw new NotFoundException(`Config with id ${id} not found`);
-    }
+    const config = await this.configsRepository.findOne({ where: fields });
     await this.cacheManager.set(cacheKey, config, 60);
     return config;
   }
 
+  async findOneOrFailed(fields: FindOptionsWhere<Config>): Promise<Config> {
+    const config = await this.findOne(fields);
+    if (!config) {
+      throw new Error('Config not found');
+    }
+
+    return config;
+  }
+
   async update(id: number, updateConfigDto: UpdateConfigDto): Promise<Config> {
-    const config = await this.findOne(id);
+    const config = await this.findOneOrFailed({ id });
+
     Object.assign(config, updateConfigDto);
+
     await this.configsRepository.save(config);
-    await this.cacheManager.del(`config_${id}`);
-    await this.cacheManager.del('configs');
+
+    this.cacheManager.del('configs::*');
     return config;
   }
 
   async remove(id: number): Promise<void> {
-    const config = await this.findOne(id);
+    const config = await this.findOneOrFailed({ id });
     await this.configsRepository.remove(config);
-    await this.cacheManager.del(`config_${id}`);
-    await this.cacheManager.del('configs');
+
+    this.cacheManager.del('configs::*');
   }
 }
