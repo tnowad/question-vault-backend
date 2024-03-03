4ad3c55e420587b2b69a22494ce05fa6787d71fa
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-03 07:51:47 +0700

    feat: add findOne for roles service

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class RolesService {
   constructor(
     @InjectRepository(Role) private readonly rolesRepository: Repository<Role>,
-  ) { }
+  ) {}
+
+  async findOne(fields: FindOptionsWhere<Role>): Promise<Role | null> {
+    return await this.rolesRepository.findOne({ where: fields });
+  }
+
+  async findOneOrFailed(fields: FindOptionsWhere<Role>): Promise<Role> {
+    const role = await this.findOne(fields);
+    if (!role) {
+      throw new Error("Can't find role");
+    }
+
+    return role;
+  }
 }
