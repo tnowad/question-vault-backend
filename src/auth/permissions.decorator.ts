d0910d7a2fb4b774637919557cd9cb06691867ba
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-05 18:45:59 +0700

    chore: remove unused import

--- src/auth/permissions.decorator.ts
+++ src/auth/permissions.decorator.ts
@@ -1,5 +1,4 @@
 import { Reflector } from '@nestjs/core';
-import { Permission } from 'src/permissions/entities/permission.entity';
 import { PermissionValue } from 'src/permissions/enums/permissions.enum';
 
 export const Permissions = Reflector.createDecorator<PermissionValue[]>();
