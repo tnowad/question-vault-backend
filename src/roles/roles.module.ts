4ae2ac912b4f546cb62f236141ecb067df96989e
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-05 17:28:45 +0700

    feat: add roles controller module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { RolesController } from './roles.controller';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
   providers: [RolesService],
   exports: [RolesService],
+  controllers: [RolesController],
 })
 export class RolesModule {}
